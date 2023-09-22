13faf447258cf83e4d346624b7e12936ea108e38
david <david.garcia2388@gmail.com>
2023-09-22 12:51:06 -0300

    22/09/2023 Version New Lector Elo intot de ejecucion auto pero no anda

---
+++ app/src/main/java/com/example/sanroque_consultor/bootapp.java
@@ -0,0 +1,21 @@
+package com.example.sanroque_consultor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+public class bootapp extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            // Inicia la actividad MainActivity al arrancar el dispositivo
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
